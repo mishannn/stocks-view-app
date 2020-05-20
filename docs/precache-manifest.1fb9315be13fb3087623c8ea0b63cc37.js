049083eb727ef56e20d9f22ec8215470e59d3fe4
Некрасов Михаил Владимирович <mvnekra7@mts.ru>
2020-05-20 15:24:03 +0300

    Change path

---
+++ docs/precache-manifest.1fb9315be13fb3087623c8ea0b63cc37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1276c6bd11905de6a043cc559512f1b1",
+    "url": "/stocks-view-app/index.html"
+  },
+  {
+    "revision": "00a2f506bfc5e01f8109",
+    "url": "/stocks-view-app/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "af68012f4fef2dcf7c78",
+    "url": "/stocks-view-app/static/css/main.418df0b3.chunk.css"
+  },
+  {
+    "revision": "00a2f506bfc5e01f8109",
+    "url": "/stocks-view-app/static/js/2.c336c164.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/stocks-view-app/static/js/2.c336c164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af68012f4fef2dcf7c78",
+    "url": "/stocks-view-app/static/js/main.144f5185.chunk.js"
+  },
+  {
+    "revision": "0635790fc974258e6e0b",
+    "url": "/stocks-view-app/static/js/runtime-main.0694587b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c99ac3303e4cdfbfa009d151e9041e6.js"
+  "/stocks-view-app/precache-manifest.1fb9315be13fb3087623c8ea0b63cc37.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stocks-view-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
